e1c082b0fcf4ff4ba6c1cb1dc90e44ac4aec93d8
Ariel Rodriguez <ariel@volonbolon.me>
2018-03-02 11:33:22 -0300

    Add custom window to silence an Xcode warning

---
+++ ConvexHull/CustomWindow.swift
@@ -0,0 +1,13 @@
+//
+//  CustomWindow.swift
+//  ConvexHull
+//
+//  Created by Ariel Rodriguez on 02/03/2018.
+//  Copyright © 2018 VolonBolon. All rights reserved.
+//
+
+import Cocoa
+
+class CustomWindow: NSWindow {
+
+}
